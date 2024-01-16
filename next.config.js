27a21295c7c6700903e123b2fb40a6bfe6e5229d
fabianommoraes <fabiano.m.moraes@gmail.com>
2024-01-16 01:28:57 -0300

    295. A Full Deployment Example (To Vercel)

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    mongodb_username: "fabianommoraes",
-    mongodb_password: "RiskOfRain2",
-    mongodb_clustername: "cluster0",
-    mongodb_database: "my-site",
-  },
-};
